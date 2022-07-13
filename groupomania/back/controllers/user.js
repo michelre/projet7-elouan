63dbac1845e1b96bb4d70d04408b641769879bab
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-07-13 18:41:46 +0200

    [FIX] the controllers for the signup and the login, create and save user, if already signup, return an error, can log and asign a token but asign the same token to all user i think, and always throw an error in the hash in signup

--- groupomania/back/controllers/user.js
+++ groupomania/back/controllers/user.js
@@ -5,15 +5,74 @@ const connection = require('../connection');
 
 const User = require('../models/user');
 
-exports.signup = async (req, res, next) => {
-  //res.status(201).json(req.body);
-  //add new user and return 201
-  const salt = await bcrypt.genSalt(10);
-  var user = {
-    name : req.body.name,
-    email : req.body.email,
-    password : await bcrypt.hash(req.body.password, salt)
-  };
-  created_user = await User.create(user);
-  res.status(201).json(created_user);
+exports.signup = (req, res, next) => {
+  User.findOne ({where: {email: req.body.email}})
+    .then(user => {
+      if (user) {
+        return res.status(409).json({ error: 'Email already exists' });
+      }
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          let user = {
+            name : req.body.name,
+            email : req.body.email,
+            password : hash
+          }
+          User.create(user);
+          User.save()
+            .then(() => res.status(201).json({message: 'User created'}))
+            .catch(error => res.status(400).json({ error}));
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
 };
+
+exports.login = (req, res, next) => {
+  User.findOne ({where: {email: req.body.email}})
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user.id,
+            token: jwt.sign(
+              { userId: user._id },
+              'secret',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+
+
+
+/* exports.signup = (req, res, next) => {
+  User.findOne ({where: {email: req.body.email}} )
+    .then(user => {
+      if (user) {
+        return res.status(409).json({ error: 'Email already exists' });
+      }
+        bcrypt.hash(req.body.password, 10)
+          .then(hash => {
+            let user = {
+              name : req.body.name,
+              email : req.body.email,
+              password : hash
+            }
+            User.create(user);
+            User.save();
+            res.status(201).json({ message: 'User registered !' });
+          })
+          .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(400).json({ error }));
+}; */
--- groupomania/back/sync-db.js
+++ groupomania/back/sync-db.js
@@ -1,3 +1,8 @@
 const db = require('./models')
 
-db.sequelize.sync({force: true})
+db.sequelize.sync({alter : true}).then((err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('Database synced')
+})
